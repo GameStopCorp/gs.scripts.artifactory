5915c3b373b53ac8f3cf49f077f23b571ae8eff7
sreinen <reinen.scott@gmail.com>
2018-05-16 09:15:29 -0500

    Corrected the registry and email config

--- src/artifactory_helper.sh
+++ src/artifactory_helper.sh
@@ -170,9 +170,10 @@ function authenticateNpm {
 
     rm -f .npmrc
 
-    npm config set registry https://gme.jfrog.io/gme/api/npm/npm
+    npm config set '@gamestop:registry' https://gme.jfrog.io/gme/api/npm/npm
     npm config set '//gme.jfrog.io/gme/api/npm/npm/:_auth' $npmToken
     npm config set '//gme.jfrog.io/gme/api/npm/npm/:always-auth' true
+    npm config set '//gme.jfrog.io/gme/api/npm/npm/:email' cibot@gamestop.com
 
     echo "----- END authenticateNpm -----"
 }
